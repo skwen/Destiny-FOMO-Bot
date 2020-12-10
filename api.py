67a6b85a770d1501a94875d75bc2c7190bcb4927
Sean Wen <sean.kcb.wen@gmail.com>
2020-12-09 21:17:07 -0500

    Dumping vendor definitions

--- api.py
+++ api.py
@@ -1,7 +1,9 @@
 api_token = '08833c369fb7488b876951c83e24c427'
 api_url_base = 'https://www.bungie.net/Platform/'
 
-oauth_code = '3e429d9aba54d6d556374f8c6009dcdb' # temp oauth code for dev
+oauth_code = '97e9d1e8e8ae585cedfd376ce6ac9911' # temp oauth code for dev
+client_id = '34928'
+access_token = 'CPPaAhKGAgAgmnbGDs0wsDQGDg05Hi8woo4tXTNeeyCcgJOAD62nhODgAAAAZEhjJoUMBh5UBpuvQSwEcM3+ijUpzuzCI6peP032/zyfw+iq50CXceyCzEZXRN5Yq26pFvvk6KgcX6FHFPbIyfkpM9MGPauhJlFtgljRs3IBbhpD2O/dwX2eFAZcDUR8hL1bbJThEWRDaXltCsxKRure9qbHUH6bkw7o58KJIriemYRQaVpyLLrWAUogE1l7HY366n23/6+uQ0sKUJEB69JmL5aAqu3GBA7cwZo5sadEJYq0lqS5vaYX39tG19hJHYCw3dTSS4MwRC7a14AulfECykyPblQwuFZMO5+KAEI='
 
-headers = {'X-API-Key': api_token}
+headers = {'X-API-Key': api_token, 'Authorization': 'Bearer {0}'.format(access_token)}
 post_data = {'code': oauth_code}
--- mods_check.py
+++ mods_check.py
@@ -16,7 +16,7 @@ def search_account(name):
 
 def get_oauth_token():
     token_uri = 'https://www.bungie.net/platform/app/oauth/token/'
-    response = requests.post(token_uri,headers=api.headers, json={'grant_type': 'authorization_code', 'code': api.oauth_code})
+    response = requests.post(token_uri,headers=api.headers, data={'grant_type': 'authorization_code', 'code': api.oauth_code, 'client_id': api.client_id})
     print(response.status_code)
     print(response.text)
 
@@ -30,18 +30,37 @@ def get_profile(membership_id):
         return None
 
 def get_vendors(membership_id, character_id):
-    vendors_uri = '{0}Destiny2/3/Profile/{1}/Character/{2}/Vendors/?components={3}'.format(api.api_url_base, membership_id, character_id, '400,402')
-    response = requests.get(vendors_uri, headers=api.headers, )
+    vendors_uri = '{0}Destiny2/3/Profile/{1}/Character/{2}/Vendors/?components={3}'.format(api.api_url_base, membership_id, character_id, '402')
+    response = requests.get(vendors_uri, headers=api.headers)
     if response.status_code == 200:
         response_object = json.loads(response.content.decode('utf-8'))
-        print(response_object)
-        # return response_object['Response']['profile']['data']['characterIds'][0]
+        vendor_file = open("vendorFile.json", "w")
+        vendor_file.write(json.dumps(response_object, indent=4, sort_keys=True))
+        vendor_file.close()
     else:
         print(response.content)
+        return None
+    return None
+
+def get_vendor_defintion(hashidentifier):
+    definition_uri = '{0}Destiny2/Manifest/DestinyVendorDefinition/{1}/'.format(api.api_url_base, hashidentifier)
+    response = requests.get(definition_uri, headers=api.headers)
+    if response.status_code == 200:
+        response_object = json.loads(response.content.decode('utf-8'))
+        vendor_file = open("vendorDefinition{0}.json".format(hashidentifier), "w")
+        vendor_file.write(json.dumps(response_object, indent=4, sort_keys=True))
+        vendor_file.close()
+    else:
+        # print(response.content)
         return None
     return None
 
 # membership_id = search_account('Hegna')
 # first_character_id = get_profile(membership_id)
 # get_vendors(membership_id, first_character_id)
-get_oauth_token()
+vendors_file = open("vendorFile.json", 'r')
+json_vendors = json.load(vendors_file)
+vendor_hashes = json_vendors['Response']['sales']['data'].keys()
+for x in vendor_hashes:
+    get_vendor_defintion(x)
+# get_oauth_token()
