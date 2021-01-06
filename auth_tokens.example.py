f165921eac0403a1c42d228485e1a416bed71426
Sean Wen <sean.kcb.wen@gmail.com>
2021-01-06 12:19:31 -0500

    Bunch of clean-up. Now gets missing mods list

---
+++ auth_tokens.example.py
@@ -0,0 +1,5 @@
+
+api_key = 'api key from bungie'
+oauth_code = 'oauth code gotten from bungie for a given account' # temp oauth code for dev
+client_id = 'the oauth client id of your dev build'
+access_token = 'access token from bungie for a given account'
