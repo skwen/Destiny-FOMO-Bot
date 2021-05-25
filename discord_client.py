9d8f4b3edef5a671646eb7deafe4747357e75181
Kyle Lexmond <sea.remote@kyl191.net>
2021-05-25 23:03:39 +0000

    Update the discord client to use the new api

--- discord_client.py
+++ discord_client.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-import discord
+import configparser
 import re
+
+from bungie_api import BungieApi
+import discord
 import mods_check
-import api
-import auth_tokens
 import DestinyEnums
 import reply_strings
 
 
 class FomoBotClient(discord.Client):
-    def __init__(self, pattern):
+    def __init__(self, pattern, bungie_api_config):
         self.pattern = pattern
+        self.api = BungieApi(bungie_api_config)
         super().__init__()
 
     async def on_ready(self):
@@ -19,25 +21,28 @@ class FomoBotClient(discord.Client):
     async def on_message(self, message):
         print("Message from {0.author}: {0.content}".format(message))
         if message.content.startswith("!mods"):
-            oauth_headers = {
-                "X-API-Key": "API KEY HERE",
-                "Authorization": "Bearer Auth Token Here",
-            }
-            membership_id = mods_check.search_account("Hegna")
-            mods_list = mods_check.banshee_has_new_mod(oauth_headers, membership_id)
+            username = message.split(" ", maxsplit=1)[-1]
+            membership_ids = self.api.search_accounts(username)
+            print(f"Found {len(membership_ids)} users matching {username}")
+            known_members = self.api.filter_unknown_memberships(membership_ids)
+            if len(known_members) == 0:
+                await message.channel.send(
+                    f"No one by the {username} is authorized with me :("
+                )
+            # Pop someone off the set of known users (in most cases, it'll be length 1)
+            membership_id = known_members.pop()
+            mods_list = mods_check.banshee_has_new_mod(self.api, membership_id)
             await message.channel.send(
-                "Banshee has the following mods you do not own: {0}".format(mods_list)
+                f"Banshee has the following mods {membership_id} does not own: {mods_list}"
             )
         elif message.content.startswith("!mention"):
             await message.channel.send(
                 "{0} here is your mention".format(message.author.mention)
             )
         elif pattern.search(message.content):
-            item_found = mods_check.item_search(
-                pattern.search(message.content).group(1)
-            )
+            item_found = self.api.item_search(pattern.search(message.content).group(1))
             # TODO(swen): Likely better to just return the string to reply with to the client, so that this class just handles discord interfacing
-            item_definition = mods_check.get_item_definition(item_found.hash)
+            item_definition = self.api.get_item_definition(item_found.hash)
             item_name = item_definition["Response"]["displayProperties"]["name"]
             item_icon = item_definition["Response"]["displayProperties"]["icon"]
             item_type = item_definition["Response"]["itemTypeAndTierDisplayName"]
@@ -59,6 +64,10 @@ class FomoBotClient(discord.Client):
             await message.channel.send(embed=embed)
 
 
-pattern = re.compile(r"\[\[(.*)\]\]")
-client = FomoBotClient(pattern)
-client.run(auth_tokens.discord_bot_token)
+if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    pattern = re.compile(r"\[\[(.*)\]\]")
+    client = FomoBotClient(pattern, bungie_api_config=config["bungie"])
+    client.run(config["discord"]["bot_token"])
