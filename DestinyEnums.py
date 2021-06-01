5577c5678de2645ef6e9e803fe4e93ce503a8ed2
Sherman Ying <fishermanying@gmail.com>
2021-06-01 00:43:06 -0400

    API usage cleanup
    - Update the instructions in the README file
    - Add support for non-Steam profiles
    - Use Bungie enum for API components rather than hardcoded magic numbers
    - Retrieve membershipType in addition to membershipId when selecting a profile

--- DestinyEnums.py
+++ DestinyEnums.py
@@ -1,6 +1,7 @@
+# See https://bungie-net.github.io/
 from enum import Enum
 
-
+# https://bungie-net.github.io/#/components/schemas/Destiny.DestinyAmmunitionType
 class DestinyAmmunitionType(Enum):
     NoType = 0
     Primary = 1
@@ -9,6 +10,7 @@ class DestinyAmmunitionType(Enum):
     Unknown = 4
 
 
+# https://bungie-net.github.io/#/components/schemas/Destiny.DamageType
 class DamageType(Enum):
     NoType = 0
     Kinetic = 1
@@ -20,3 +22,53 @@ class DamageType(Enum):
 
 class D2Vendors(Enum):
     Banshee = 672118013
+
+# https://bungie-net.github.io/#/components/schemas/BungieMembershipType
+class BungieMembershipType(Enum):
+    All = -1
+    NoType = 0
+    TigerXbox = 1
+    TigerPsn = 2
+    TigerSteam = 3
+    TigerBlizzard = 4
+    TigerStadia = 5
+    TigerDemon = 10
+    BungieNext = 254
+
+# https://bungie-net.github.io/#/components/schemas/Destiny.DestinyComponentType
+class DestinyComponentType(Enum):
+    NoType = 0
+    Profiles = 100
+    VendorReceipts = 101
+    ProfileInventories = 102
+    ProfileCurrencies = 103
+    ProfileProgression = 104
+    PlatformSilver = 105
+    Characters = 200
+    CharacterInventories = 201
+    CharacterProgressions = 202
+    CharacterRenderData = 203
+    CharacterActivities = 204
+    CharacterEquipment = 205
+    ItemInstances = 300
+    ItemObjectives = 301
+    ItemPerks = 302
+    ItemRenderData = 303
+    ItemStats = 304
+    ItemSockets = 305
+    ItemTalentGrids = 306
+    ItemCommonData = 307
+    ItemPlugStates = 308
+    ItemPlugObjectives = 309
+    ItemReusablePlugs = 310
+    Vendors = 400
+    VendorCategories = 401
+    VendorSales = 402
+    Kiosks = 500
+    CurrencyLookups = 600
+    PresentationNodes = 700
+    Collectibles = 800
+    Records = 900
+    Transitory = 1000
+    Metrics = 1100
+    StringVariables = 1200
