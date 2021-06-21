867a03f528eedb70dd2d260117f5637cd53ff91c
Sherman Ying <fishermanying@gmail.com>
2021-06-21 14:11:53 -0400

    secret_config.py should use 'config.ini' as the fallback CONFIG_PATH

--- secret_config.py
+++ secret_config.py
@@ -3,9 +3,9 @@ import os
 
 def read():
     """
-    Read secrets/variables from a file mount
+    Read secrets/variables from a file mount, default to './config.ini'
     """
-    CONFIG_PATH = os.getenv('CONFIG_PATH')
+    CONFIG_PATH = os.getenv('CONFIG_PATH', 'config.ini')
     config = configparser.ConfigParser()
     config.read(CONFIG_PATH)
     return config
